03b2f07dbba8e701a3b0f8a32707ca501a264bb3
nsuzuki7713 <nsuzuki7713@gmail.com>
2020-05-10 22:55:27 +0900

    明細票変換処理の追加

--- README.md
+++ README.md
@@ -4,6 +4,13 @@
 
 言語はTypeScriptです。
 
+## スクリプト概要
+
+### src/convertSmbcSpecification
+
+三井住友銀行の明細表を使う形に変換する  
+プロジェクトの直下にダウンロードした`meisai.csv`を置く。
+
 ## 参考URL
 
 - [TypeScript + Node.js プロジェクトのはじめかた2019](https://qiita.com/notakaos/items/3bbd2293e2ff286d9f49)
---
+++ src/convertSmbcSpecification.ts
@@ -0,0 +1,53 @@
+import fs from 'fs';
+import iconv from 'iconv-lite';
+
+const file = fs.readFileSync('./meisai.csv');
+const text = iconv.decode(Buffer.from(file), 'Shift_JIS');
+const details = text.split('\r\n').slice(1);
+
+fs.unlinkSync('./convertMeisai.csv');
+fs.writeFileSync('./convertMeisai.csv', `年\t月\t日\tお引出し\tお預入れ\tお取り扱い内容\t残高\t詳細\r\n`);
+details.map((detail) => {
+  const items = detail.split(',');
+  const date = items[0].split('.');
+
+  const outPut = [
+    convertWarekiToSeireki(date[0]),
+    Number(date[1]),
+    date[2],
+    ...items.slice(1),
+    convertUse(items[2], items[3]),
+  ].join('\t');
+
+  fs.appendFileSync('./convertMeisai.csv', `${outPut}\r\n`);
+});
+
+function convertWarekiToSeireki(wareki: string): number {
+  switch (wareki) {
+    case 'R01':
+      return 2019;
+    case 'R02':
+      return 2020;
+    case 'R03':
+      return 2021;
+    case 'R04':
+      return 2022;
+    case 'R05':
+      return 2023;
+    default:
+      return 9999;
+  }
+}
+
+function convertUse(deposit: string, content: string): string {
+  if (deposit) {
+    return '入金';
+  }
+
+  const funds = ['"MHF)ｳｴﾙｽﾅﾋﾞ"', '"AP(ﾏﾈﾂｸｽﾂﾐﾀﾃ"', '"ｺｸﾐﾝﾈﾝｷﾝｷｷﾝﾚﾝｺﾞｳｶｲ(ｶ"', '"ｼﾖｳｷﾎﾞｶｹｷﾝ"', '"ﾏﾈﾂｸｽｼﾖｳｹﾝ"'];
+  if (funds.includes(content)) {
+    return '資金移動';
+  }
+
+  return '支払い';
+}
