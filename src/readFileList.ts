b2d383c8c756322e29b94441d49ad278eeb85fcc
nsuzuki7713 <nsuzuki7713@gmail.com>
2020-08-20 23:49:41 +0900

    再帰的なファイル検索をする

--- src/readFileList.ts
+++ src/readFileList.ts
@@ -16,9 +16,29 @@ const func1 = (dirPath: string) => {
   console.log(fileNames);
 };
 
+// dirPath以下を再帰的に検索してのfile一覧を取得する
+const func2 = (dirPath: string): { dir: string; name: string }[] => {
+  const allDirents = fs.readdirSync(dirPath, { withFileTypes: true });
+
+  const files = [];
+  for (const dirent of allDirents) {
+    if (dirent.isDirectory()) {
+      const fp = path.join(dirPath, dirent.name);
+      files.push(func2(fp));
+    } else if (dirent.isFile() && ['.txt'].includes(path.extname(dirent.name))) {
+      files.push({
+        dir: path.join(dirPath, dirent.name),
+        name: dirent.name,
+      });
+    }
+  }
+  return files.flat();
+};
+
 (async () => {
   const dirPath = './__tests__/readFileList';
-  func1(dirPath);
+  // func1(dirPath);
+  console.log(func2(dirPath));
 })().catch((e) => {
   console.log(e);
 });
