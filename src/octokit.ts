46fa01d2ca6011607d04847e0df9bb8997e469fc
nsuzuki7713 <nsuzuki7713@gmail.com>
2020-08-29 23:41:13 +0900

    プルリクのマージ内容一覧を取得

--- src/octokit.ts
+++ src/octokit.ts
@@ -11,11 +11,7 @@ const owner = 'nsuzuki7713';
 const repo = 'typescript-node-adhoc-scripts';
 
 const fetchShaFromBranch = async (branch: string): Promise<string> => {
-  const { data } = await octokit.repos.getBranch({
-    owner,
-    repo,
-    branch,
-  });
+  const { data } = await octokit.repos.getBranch({ owner, repo, branch });
   return data.commit.sha;
 };
 
@@ -39,8 +35,29 @@ const createTag = async (branch: string, tagName: string) => {
   });
 };
 
+const getPullRequestMergingList = async (base: string, head: string): Promise<PullRequestList[]> => {
+  const response = await octokit.repos.compareCommits({ owner, repo, base, head });
+  return response.data.commits.reduce((acc, cur) => {
+    const [title, , ...body] = cur.commit.message.split('\n');
+    const result = title.match(/Merge pull request (#.)/);
+    if (result) {
+      acc.push({
+        pullRequestNumber: result[1],
+        message: body[0],
+      });
+    }
+    return acc;
+  }, [] as PullRequestList[]);
+};
+
 (async () => {
-  await createTag('master', `test-tag-${Date.now()}`);
+  // await createTag('master', `test-tag-${Date.now()}`);
+  console.log(await getPullRequestMergingList('test-tag-1598708009047', 'master'));
 })().catch((e) => {
   console.log(e);
 });
+
+interface PullRequestList {
+  pullRequestNumber: string;
+  message: string;
+}
